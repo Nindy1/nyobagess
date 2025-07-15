8ee64b8ec9ae2353ce95e7ee27e5d580e3233c53
Nindy1 <nindyaghinaramadhani@gmail.com>
2025-07-15 15:52:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ bahan_kimia = {
     "Tindakan Pencegahan": "Gunakan APD lengkap, hindari inhalasi.",
     "Penyimpanan": "Simpan di tempat gelap dan dingin."
   },
- "Asam asetat":{
+ "Asamasetat":{
     "Rumus": "CH3COOH",
     "Bahaya": "Iritasi kulit dan mata, korosif pada konsentrasi tinggi.",
     "Tindakan Pencegahan": "Gunakan sarung tangan dan pelindung mata.",
